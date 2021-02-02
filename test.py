4a549c11ea4bd4e5451510726da2e58d2f10a4f9
soltan170 <78342876+soltan170@users.noreply.github.com>
2021-02-03 02:25:16 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+
+def main():
+    # front end elements of the web page
+    html_temp = """ 
+    <div style ="background-color:cyan;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Streamlit MCO effective measure  App</h1> 
+    </div> 
+    """
+
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    # following lines create boxes in which user can enter data required to make prediction
+    LockDownType = st.selectbox('Lock down Type', ("PKP", "PKPB","PKPP"))
+    CaseNumber = st.number_input("number of cases")
+    DateRange = st.number_input("number of date enforced")
+    SocialDistancing = st.number_input("Social Distance rate")
+    PeriodEnforce = st.number_input("Period enforced")
+
+
+    result = ""
+
+    # when 'Predict' is clicked, make the prediction and store it
+    if st.button("Predict"):
+        # prediction method calling
+        #result =
+        st.success('The MCO status: {}'.format(result))
+
+
+if __name__=='__main__':
+    main()
