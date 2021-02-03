f9e5a3bed2d0e18ee1fee1d5a805c105085f1137
Ikhwan Salihin <ikhwanvodka@gmail.com>
2021-02-04 00:33:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,50 +7,85 @@ import pickle
 import sklearn
 
 
-def predict(LockDownType, CaseNumber, DateRange, SocialDistancing, PeriodEnforce):
-    # loading the trained model
-    pickle_in = open('classifier.pkl', 'rb')
-    classifier = pickle.load(pickle_in)
-    if LockDownType == 'PKP':
+# loading the trained model
+pickle_in = open('myClassifier.pkl', 'rb') 
+classifier = pickle.load(pickle_in)
+ 
+@st.cache()
+  
+# defining the function which will make the prediction using the data which the user inputs 
+def prediction(lockdown_types, new_cases, total_case, date_range, r_naught ):   
+ 
+    
+    
+    if lockdown_types == "PKP":
+        lockdown_types = 0
+        location = 0
+    elif lockdown_types == "PKPB":
         lockdown_types = 1
-
-    date_range = DateRange
-    social_distancing_rate = SocialDistancing
-    cases = CaseNumber
-    period_enforce = PeriodEnforce
-
-
-    prediction = classifier.predict(
-        [[lockdown_types, date_range, social_distancing_rate, cases, period_enforce]])
-    return prediction
-
-def main():
-    # front end elements of the web page
+        location = 0
+    elif lockdown_types == "PKPP":
+        lockdown_types = 2
+        location = 0
+    elif lockdown_types == "SingaporePrelude":
+        lockdown_types = 3
+        location = 1
+    elif lockdown_types == "SingaporeCB":
+        lockdown_types = 4
+        location = 1
+    elif lockdown_types == "SingaporePhase1":
+        lockdown_types = 5
+        location = 1
+    elif lockdown_types == "SingaporePhase2":
+        lockdown_types = 5
+        location = 1
+    elif lockdown_types == "SingaporePhase3":
+        lockdown_types = 5
+        location = 1
+    else :
+        lockdown_types = 6
+        location = 2
+    # Making predictions 
+    prediction = classifier.predict( 
+        [[location, new_cases, total_case, lockdown_types, date_range, r_naught]])
+     
+    if prediction == 0:
+        pred = 'Effective'
+    else:
+        pred = 'Not Effective'
+    return pred
+      
+  
+# this is the main function in which we define our webpage  
+def main():       
+    # front end elements of the web page 
     html_temp = """ 
-    <div style ="background-color:cyan;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit MCO effective measure  App</h1> 
+    <div style ="background-color:gray;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Lockdown Effectiveness Prediction ML App</h1> 
     </div> 
     """
-
+      
     # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html=True)
-
-    # following lines create boxes in which user can enter data required to make prediction
-    LockDownType = st.selectbox('Lock down Type', ("PKP", "PKPB","PKPP"))
-    CaseNumber = st.number_input("number of cases")
-    DateRange = st.number_input("number of date enforced")
-    SocialDistancing = st.number_input("Social Distance rate")
-    PeriodEnforce = st.number_input("Period enforced")
-
-
-    result = ""
-
-    # when 'Predict' is clicked, make the prediction and store it
-    if st.button("Predict"):
-        # prediction method calling
-        result = predict(LockDownType, CaseNumber, DateRange, SocialDistancing, PeriodEnforce)
-        st.success('The MCO status: {}'.format(result))
-
-
-if __name__=='__main__':
+    st.markdown(html_temp, unsafe_allow_html = True) 
+      
+    # following lines create boxes in which user can enter data required to make prediction 
+    lockdown_types = st.selectbox('Lockdown Types',("PKP","PKPB","PKPP","SingaporePrelude","SingaporeCB","SingaporePhase1","SingaporePhase2"))
+    new_cases = st.number_input('Today Cases', min_value=0, max_value=1500)
+    previous_case = st.number_input('Previous Day Cases', min_value=0, max_value=1500)
+    total_case = st.number_input('Total Cases', min_value=0)
+    date_range = st.slider('Date Range', min_value=0, max_value=365)
+    
+    if previous_case == 0 :
+        r_naught = 0
+    else :
+        r_naught = round(new_cases/previous_case,2)
+    
+    result =""
+    # when 'Predict' is clicked, make the prediction and store it 
+    if st.button("Predict"): 
+        result = prediction(lockdown_types, new_cases, total_case, date_range, r_naught)
+        st.success('Lockdown is {}'.format(result))
+        
+     
+if __name__=='__main__': 
     main()
