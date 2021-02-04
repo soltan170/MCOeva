2a09776c647bb4ced91186c242be160064c6b8e0
Ikhwan Salihin <ikhwanvodka@gmail.com>
2021-02-04 23:02:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import pandas as pd
 import pickle
 import sklearn
 import time
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
 
 # loading the trained model
 pickle_in = open('randomForest.pkl', 'rb') 
@@ -103,7 +106,20 @@ def main():
         else :
          st.error('Lockdown is {} '.format(result))
         
- 
+     b = (
+         Bar()
+         .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+         .add_yaxis(
+             "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
+         )
+         .set_global_opts(
+             title_opts=opts.TitleOpts(
+                 title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+             ),
+             toolbox_opts=opts.ToolboxOpts(),
+         )
+     )
+     st_pyecharts(b)
      
 if __name__=='__main__': 
     main()
